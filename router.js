7db3b968f7430dc1f1a0f1a2299282431b689de4
cutiefunny <cutiefunny@gmail.com>
2022-01-04 00:40:33 +0900

    on map

--- router.js
+++ router.js
@@ -5,9 +5,10 @@ exports.main = function(req,res) {
 
     fs.readdir(__dirname+'/images/', function(err,fileList){
         var name = getCat(fileList);
-        console.log(name);
+        console.log(fileList);
             res.render('main', { 
                 title: 'cat for all'
+                , filelist : fileList
                 , name : name
                 , tags : "no tags"
             });
--- script/main.js
+++ script/main.js
@@ -3,11 +3,12 @@ var menu = document.getElementById("menu");
 var div_sideBar = document.getElementById("div_sideBar");
 var img_main = document.getElementById("img_main");
 var img_name = document.getElementById("img_name");
+var filelist = document.getElementsByName("file");
 //#endregion
 
 //페이지 시작 시 수행되는 함수
 window.onload = function(){
-
+    setMarkers(filelist);
 };
 
 //메뉴 클릭
@@ -16,26 +17,59 @@ function change(){ callAjax("change") }
 function clickThumb(src){ img_main.setAttribute("src",src); }
 function clickNext(name) { alert(name); }
 
+let markers = new Array();
+
 var HOME_PATH = window.HOME_PATH || '.';
-var position = new naver.maps.LatLng(37.3849483, 127.1229117);
+var position = new naver.maps.LatLng(37.546219, 126.919683);
 
 var mapOptions = {
     center: position,
-    zoom: 10
+    zoom: 17
 };
 
 var map = new naver.maps.Map('map', mapOptions);
 
 var markerOptions = {
-    position: position.destinationPoint(90, 15),
+    position: position.destinationPoint(0, 0),
     map: map,
     icon: {
         url: HOME_PATH+'/images/'+img_name.getAttribute('value'),
         //size: new naver.maps.Size(50, 52),
-        scaledSize: new naver.maps.Size(50, 52),
+        scaledSize: new naver.maps.Size(map.zoom*3, map.zoom*3),
         origin: new naver.maps.Point(0, 0),
         anchor: new naver.maps.Point(25, 26)
     }
 };
 
 var marker = new naver.maps.Marker(markerOptions);
+
+markers.push(marker);
+
+function setMarkers(filelist){
+
+    var x;
+    var y;
+
+    filelist.forEach(file => {
+        x=file.getAttribute("value").split("_")[0];
+        y=file.getAttribute("value").split("_")[1].split(".")[0]+"."+file.getAttribute("value").split("_")[1].split(".")[1];
+
+        position = new naver.maps.LatLng(x, y);
+
+        var markerOptions = {
+            position: position.destinationPoint(0, 0),
+            map: map,
+            icon: {
+                url: HOME_PATH+'/images/'+file.getAttribute("value"),
+                // scaledSize: new naver.maps.Size(50, 52),
+                scaledSize: new naver.maps.Size(map.zoom*3, map.zoom*3),
+                origin: new naver.maps.Point(0, 0),
+                anchor: new naver.maps.Point(25, 26)
+            }
+        };
+        
+        var marker = new naver.maps.Marker(markerOptions);
+        
+        markers.push(marker);
+    });
+}
